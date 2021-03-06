4668e22f006c688c8056bb0ffc789902396194b3
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-03-05 20:21:30 -0700

    Update variables

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // connect to db
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_CLOUD, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
