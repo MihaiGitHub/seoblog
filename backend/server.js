874a108b5aade9310b80927854da61cf17da12cd
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-03-05 17:05:21 -0700

    Setup Node.js server

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// bring in all the packages
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// create app
+const app = express();
+
+// add middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// add routes
+app.get("/api", (req, res) => {
+  res.json({ time: Date().toString() });
+});
+
+// use environment port
+const port = process.env.PORT || 8000;
+
+// run app
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
