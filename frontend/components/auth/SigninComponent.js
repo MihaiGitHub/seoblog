99ea221f7ce844e0e07d8c29fe9152943eda7396
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-03-11 17:49:51 -0700

    Signin feature

---
+++ frontend/components/auth/SigninComponent.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { signin } from "../../actions/auth";
+import Router from "next/router";
+
+const SigninComponent = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true,
+  });
+
+  const { email, password, error, loading, message, showForm } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setValues({ ...values, loading: true, error: false });
+
+    const user = { email, password };
+
+    signin(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        // save user token to cookie
+
+        // save user info to localstorage
+
+        // authenticate user
+
+        // comes with Next.js
+        Router.push(`/`);
+      }
+    });
+  };
+
+  const handleChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const showLoading = () =>
+    loading ? <div className="alert alert-info">Loading...</div> : "";
+
+  const showError = () =>
+    error ? <div className="alert alert-info">{error}</div> : "";
+
+  const showMessage = () =>
+    message ? <div className="alert alert-info">{message}</div> : "";
+
+  const signinForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={email}
+            className="form-control"
+            type="email"
+            placeholder="Type your email"
+            onChange={handleChange("email")}
+          />
+          <input
+            value={password}
+            className="form-control"
+            type="password"
+            placeholder="Type your password"
+            onChange={handleChange("password")}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Signin
+        </button>
+      </form>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      {showError()}
+      {showLoading()}
+      {showMessage()}
+      {showForm && signinForm()}
+    </React.Fragment>
+  );
+};
+
+export default SigninComponent;
