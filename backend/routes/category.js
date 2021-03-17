e72dfe0db1ce479ca341dee6a12b4a40593e2485
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-03-16 17:34:26 -0700

    Category CRUD

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { requireSignin, adminMiddleware } = require("../controllers/auth");
 const router = express.Router();
-const { create } = require("../controllers/category");
+const { create, list, read, remove } = require("../controllers/category");
 
 // validators
 const { runValidation } = require("../validators");
 const { categoryCreateValidator } = require("../validators/category");
 
+// no category update route because the search engine will index the categories so
+// you don't want to change them later; Will impact SEO negatively!
 router.post(
   "/category",
   categoryCreateValidator,
@@ -14,6 +16,14 @@ router.post(
   requireSignin,
   adminMiddleware, // only admin can create a new category
   create
+);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.delete(
+  "/category/:slug",
+  requireSignin,
+  adminMiddleware, // only admin can delete a category
+  remove
 );
 
 module.exports = router;
