0456bf22ba9c59e4dbbd66447ef1ce74379d9fa7
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-03-20 22:01:41 -0700

    Create blog controller function

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -1,3 +1,60 @@
-exports.time = (req, res) => {
-  res.json({ time: Date().toString() });
+const Blog = require("../models/blog");
+const formidable = require("formidable");
+const slugify = require("slugify");
+// strip HTML and create exerpt
+const stripHtml = require("string-strip-html");
+const _ = require("lodash");
+const Category = require("../models/category");
+const Tag = require("../models/tag");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const fs = require("fs");
+
+exports.create = (req, res) => {
+  // get form data from request
+  let form = new formidable.IncomingForm();
+
+  // if files are in form data keep the extensions
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not upload",
+      });
+    }
+
+    const { title, body, categories, tags } = fields;
+
+    let blog = new Blog();
+
+    blog.title = title;
+    blog.body = body;
+    blog.slug = slugify(title).toLowerCase();
+    blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+    blog.mdesc = stripHtml(body.substring(0, 160));
+    blog.postedBy = req.user._id;
+
+    if (files.photo) {
+      if (files.photo.size > 10000000) {
+        // > 1 Mb
+        return res.status(400).json({
+          error: "Image should be less then 1mb in size",
+        });
+      }
+
+      // create a blog
+      blog.photo.data = fs.readFileSync(files.photo.path);
+      blog.photo.contentType = files.photo.type;
+    }
+
+    blog.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+
+      res.json(result);
+    });
+  });
 };
--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { time } = require("../controllers/blog");
+const { create } = require("../controllers/blog");
+const { adminMiddleware, requireSignin } = require("../controllers/auth");
 
-router.get("/", time);
+router.get("/blog", requireSignin, adminMiddleware, create);
 
 module.exports = router;
