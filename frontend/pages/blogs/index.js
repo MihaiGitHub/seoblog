190cbb5fee7811a49b23c4db27617f08ff387040
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-04-10 22:30:34 -0700

    Server side rendering of Blogs

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -5,7 +5,8 @@ import Layout from "../../components/Layout";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import { API } from "../../config";
 
-const Blogs = () => {
+// props accessible from getInitialProps
+const Blogs = ({ blogs, categories, tags, size }) => {
   return (
     <Layout>
       <main>
@@ -23,12 +24,32 @@ const Blogs = () => {
         </div>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-12">show all blogs</div>
+            {/* rendered server side */}
+            <div className="col-md-12">{JSON.stringify(blogs)}</div>
           </div>
         </div>
       </main>
     </Layout>
   );
+};
+
+// method executed server side
+// server render the page (can only be used on pages, not in components)
+Blogs.getInitialProps = () => {
+  // make request to backend, get data, and return data
+  return listBlogsWithCategoriesAndTags().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      // if have nothing to return, return {}
+      return {
+        blogs: data.blogs,
+        categories: data.categories,
+        tags: data.tags,
+        size: data.size,
+      };
+    }
+  });
 };
 
 export default Blogs;
