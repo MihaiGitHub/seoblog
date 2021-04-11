729fad4541764313d0c3d8c2d6c95e9b5e9f5ce9
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-04-11 16:49:01 -0700

    Display list of blogs

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -4,9 +4,51 @@ import Link from "next/link";
 import Layout from "../../components/Layout";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import { API } from "../../config";
+import renderHTML from "react-render-html";
+import moment from "moment";
 
 // props accessible from getInitialProps
 const Blogs = ({ blogs, categories, tags, size }) => {
+  console.log("myblogs ", blogs);
+
+  const showAllBlogs = () => {
+    console.log("showAllBlogs ", blogs);
+
+    return blogs.map((blog, i) => {
+      return (
+        <article key={i}>
+          <div className="lead pb-4">
+            <header>
+              <Link href={`/blogs/${blog.slug}`}>
+                <a>
+                  <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
+                </a>
+              </Link>
+            </header>
+            <section>
+              <p className="mark ml-1 pt-2 pb-2">
+                Written by {blog.postedBy.name} | Published&nbsp;
+                {moment(blog.updatedAt).fromNow()}
+              </p>
+            </section>
+            <div className="row">
+              <div className="col-md-4">image</div>
+              <div className="col-md-8">
+                <section>
+                  <div className="pb-3">{renderHTML(blog.excerpt)}</div>
+                  <Link href={`/blogs/${blog.slug}`}>
+                    <a className="btn btn-primary pt-2">Read more</a>
+                  </Link>
+                </section>
+              </div>
+            </div>
+          </div>
+          <hr />
+        </article>
+      );
+    });
+  };
+
   return (
     <Layout>
       <main>
@@ -25,7 +67,7 @@ const Blogs = ({ blogs, categories, tags, size }) => {
         <div className="container-fluid">
           <div className="row">
             {/* rendered server side */}
-            <div className="col-md-12">{JSON.stringify(blogs)}</div>
+            <div className="col-md-12">{showAllBlogs()}</div>
           </div>
         </div>
       </main>
@@ -41,6 +83,7 @@ Blogs.getInitialProps = () => {
     if (data.error) {
       console.log(data.error);
     } else {
+      console.log("blogs ", data.blogs);
       // if have nothing to return, return {}
       return {
         blogs: data.blogs,
