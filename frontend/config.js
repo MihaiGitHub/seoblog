bfec851887ae45a006e74272b44465b49a984c93
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-04-24 17:42:17 -0700

    Update config file

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,11 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://seoblog.com"
-  : "http://localhost:8000/api";
+  ? publicRuntimeConfig.API_PRODUCTION
+  : publicRuntimeConfig.API_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.API_PRODUCTION
+  : publicRuntimeConfig.API_DEVELOPMENT;
