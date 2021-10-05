d84ae2e38ee047e51f796536e93e1c1cec652363
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-10-04 20:45:33 -0700

    Load all blogs in BlogRead component

--- frontend/components/crud/BlogsRead.js
+++ frontend/components/crud/BlogsRead.js
@@ -5,6 +5,25 @@ import { getCookie, isAuth } from "../../actions/auth";
 import { list, removeBlog } from "../../actions/blog";
 
 const BlogRead = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const token = getCookie("token");
+
+  useEffect(() => {
+    loadBlogs();
+  }, []);
+
+  const loadBlogs = () => {
+    list().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setBlogs(data);
+      }
+    });
+  };
+
   return (
     <React.Fragment>
       <div>update delete blogs</div>
