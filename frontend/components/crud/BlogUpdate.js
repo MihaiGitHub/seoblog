00fc9da95002ba52061e51ff5187af03d537daeb
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-11-01 19:58:20 -0700

    Show categories and tags

--- frontend/components/crud/BlogUpdate.js
+++ frontend/components/crud/BlogUpdate.js
@@ -16,6 +16,11 @@ import { QuillModules, QuillFormats } from "../../helpers/quill";
 
 const BlogUpdate = ({ router }) => {
   const [body, setBody] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const [checked, setChecked] = useState([]); // categories
+  const [checkedTag, setCheckedTag] = useState([]); // tags
   const [values, setValues] = useState({
     title: "",
     error: "",
@@ -30,6 +35,8 @@ const BlogUpdate = ({ router }) => {
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
     initBlog();
+    initCategories();
+    initTags();
   }, [router]); // anytime the router changes (page refreshes) run this
 
   const initBlog = (slug) => {
@@ -40,9 +47,151 @@ const BlogUpdate = ({ router }) => {
         } else {
           setValues({ ...values, title: data.title });
           setBody(data.body);
+          setCategoriesArray(data.categories);
+          setTagsArray(data.tags);
         }
       });
     }
+  };
+
+  const setCategoriesArray = (blogCategories) => {
+    let ca = [];
+
+    blogCategories.map((c, i) => {
+      ca.push(c._id);
+    });
+
+    setChecked(ca);
+  };
+
+  const setTagsArray = (blogTags) => {
+    let ta = [];
+
+    blogTags.map((t, i) => {
+      ta.push(t._id);
+    });
+
+    setCheckedTag(ta);
+  };
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
+
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: "" });
+
+    // return the first index or -1 if it does not find the category
+    const clickedCategory = checked.indexOf(c);
+
+    // whatever is in state store in this variable
+    const all = [...checked];
+
+    // if the category id doesn't exist in the state push to this variable
+    if (clickedCategory == -1) {
+      all.push(c);
+    } else {
+      // if its already there then remove the category id from the array
+      all.splice(clickedCategory, 1);
+    }
+
+    setChecked(all);
+
+    // update form data for sending to backend
+    formData.set("categories", all);
+  };
+
+  const handleTagsToggle = (t) => () => {
+    setValues({ ...values, error: "" });
+
+    // return the first index or -1 if it does not find the category
+    const clickedTag = checkedTag.indexOf(t);
+
+    // whatever is in state store in this variable
+    const all = [...checked];
+
+    // if the category id doesn't exist in the state push to this variable
+    if (clickedTag == -1) {
+      all.push(t);
+    } else {
+      // if its already there then remove the category id from the array
+      all.splice(clickedTag, 1);
+    }
+
+    setCheckedTag(all);
+
+    // update form data for sending to backend
+    formData.set("tags", all);
+  };
+
+  const findOutCategory = (c) => {
+    const result = checked.indexOf(c); // if it doesn't exist it will return -1, else true
+
+    if (result !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const findOutTag = (t) => {
+    const result = checkedTag.indexOf(t); // if it doesn't exist it will return -1, else true
+
+    if (result !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((c, i) => (
+        <li key={i} className="list-unstyled">
+          <input
+            onChange={handleToggle(c._id)}
+            checked={findOutCategory(c._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((t, i) => (
+        <li key={i} className="list-unstyled">
+          <input
+            onChange={handleTagsToggle(t._id)}
+            checked={findOutTag(t._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{t.name}</label>
+        </li>
+      ))
+    );
   };
 
   const handleChange = (name) => (e) => {
@@ -99,13 +248,41 @@ const BlogUpdate = ({ router }) => {
       <div className="row">
         <div className="col-md-8">
           {updateBlogForm()}
-          <div className="pt-3">show success and error message</div>
+          <div className="pt-3">
+            <p>show success and error message</p>
+            <hr />
+          </div>
         </div>
         <div className="col-md-4">
           <div>
             <div className="form-group pb-b">
               <h5>Featured Image</h5>
+              <hr />
+              <small className="text-muted">Max size: 1mb</small>
+              <label className="btn btn-outline-info">
+                Upload featured image
+                <input
+                  onChange={handleChange("photo")}
+                  type="file"
+                  accept="image/*"
+                  hidden
+                />
+              </label>
             </div>
+          </div>
+          <div>
+            <h5>Categories</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
           </div>
         </div>
       </div>
