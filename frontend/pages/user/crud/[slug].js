17caf18d265da7ea7839eec539cf82a03bec2a4a
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-12-11 19:30:16 -0700

    Create update blog page

---
+++ frontend/pages/user/crud/[slug].js
@@ -0,0 +1,23 @@
+import Layout from "../../../components/Layout";
+import Private from "../../../components/auth/Private";
+import Link from "next/link";
+import BlogUpdate from "../../../components/crud/BlogUpdate";
+
+const Blog = () => {
+  return (
+    <Layout>
+      <Private>
+        <div className="row">
+          <div className="col-md-12 pt-5 pb-5">
+            <h2>Update blog</h2>
+          </div>
+          <div className="col-md-12">
+            <BlogUpdate />
+          </div>
+        </div>
+      </Private>
+    </Layout>
+  );
+};
+
+export default Blog;
