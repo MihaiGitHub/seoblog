b9fe29bc5c8d2482315cc5d8e54efa40e340e033
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-12-12 21:18:36 -0700

    Show redirect message in signin component

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,11 +1,22 @@
 import Layout from "../components/Layout";
+import { withRouter } from "next/router";
 import SigninComponent from "../components/auth/SigninComponent";
 import Link from "next/link";
 
-const Signin = () => {
+const Signin = ({ router }) => {
+  const showRedirectMessage = () => {
+    if (router.query.message) {
+      return <div className="alert alert-danger">{router.query.message}</div>;
+    } else {
+      return;
+    }
+  };
   return (
     <Layout>
       <h2 className="text-center pt-4 pb-4">Signin Page</h2>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">{showRedirectMessage()}</div>
+      </div>
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <SigninComponent />
@@ -15,4 +26,4 @@ const Signin = () => {
   );
 };
 
-export default Signin;
+export default withRouter(Signin);
