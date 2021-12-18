8c579636f5626b05b79d6a2a942ac34d7b4a8ed2
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-12-17 22:28:09 -0700

    Sending email with SendGrid

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -1,3 +1,25 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 exports.contactForm = (req, res) => {
-  res.send("contact endpoint");
+  const { email, name, message } = req.body;
+
+  const emailData = {
+    to: process.env.EMAIL_TO,
+    from: email,
+    subject: `Contact Form - ${process.env.APP_NAME}`,
+    text: `Email received from contact form \n Sender name: ${name} \n Sender email: ${email} \n Sender message: ${message}`,
+    html: `<h4>Email received from contact form</h4>
+    <p>Sender name: ${name}</p>
+    <p>Sender email: ${email}</p>
+    <p>Sender message: ${message}</p>
+    <hr />
+    <p>This email may contain sensitive information</p>`,
+  };
+
+  sgMail.send(emailData).then((sent) => {
+    return res.json({
+      success: true,
+    });
+  });
 };
