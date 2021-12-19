7a6f1b89ab7295f5e0648419014bf5cc0447cd33
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-12-18 22:01:27 -0700

    Create ContactForm component

---
+++ frontend/components/form/ContactForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { emailContactForm } from "../../actions/form";
+
+const ContactForm = () => {
+  return (
+    <React.Fragment>
+      <p>Show contact form</p>
+    </React.Fragment>
+  );
+};
+
+export default ContactForm;
