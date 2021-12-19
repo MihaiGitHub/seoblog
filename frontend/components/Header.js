4d26ed4d292517c83cd4f91f08e32db99e5b8c7e
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-12-18 22:09:37 -0700

    Update Header component with new link

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -48,6 +48,11 @@ const Header = () => {
                   <NavLink>Blogs</NavLink>
                 </Link>
               </NavItem>
+              <NavItem>
+                <Link href="/contact">
+                  <NavLink>Contact</NavLink>
+                </Link>
+              </NavItem>
             </React.Fragment>
             {!isAuth() && (
               <React.Fragment>
